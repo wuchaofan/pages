caf69143f8243d917ca32a7dd8a86d35e349b2c5
chaofan <w915546302@yahoo.cn>
2017-11-21 18:53:12 +0800

    Update mypromise.js

--- mypromise.js
+++ mypromise.js
@@ -57,5 +57,27 @@ class MyPromise{
    }
    static all(promiseList){
       //to do....
+    let values = Array.prototype.slice.call(promiseList)
+        
+    return new MyPromise(function(resolve, reject){
+      let len = promiseList.length
+
+      function res(i, item){
+         if (item instanceof MyPromise && item.then === MyPromise.prototype.then) {
+            item.then(function (val) {
+                res(i, val);
+            }, reject)
+            return
+         }
+         values[i] = item
+         len -= 1
+         if(len === 0){
+           resolve(values)
+         }
+       }
+       for (let i = 0; i < promiseList.length; i++) {
+         res(i, promiseList[i])
+       }
+     })
    }
 }
